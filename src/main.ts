8a299d22343182fd1ba0327b50e8c73dcbb93b98
Yuri Kravzov <ukravzov@mail.ru>
2024-07-11 19:00:31 +0500

    allow cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 	app.setGlobalPrefix('api')
 	app.use(cookieParser())
 	app.enableCors({
-		origin: ['http://localhost:3000'],
+		origin: ['https://yury08.github.io/yk-planner-client/'],
 		credentials: true,
 		exposedHeaders: 'set-cookie'
 	})
